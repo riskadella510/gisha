92817915419b23886ef5e11d685ab8c489d9e046
Rudy Syahnaya <rudysyahnaya@gmail.com>
2024-10-22 08:35:28 +0700

    Pertama

---
+++ resources/views/components/peta/header.blade.php
@@ -0,0 +1,51 @@
+<style>
+    .nav-link.active {
+        color: #D95639 !important;
+    }
+
+    .nav-link:hover {
+        color: #D95639 !important;
+    }
+</style>
+
+<nav class="navbar">
+    <a href="#" class="sidebar-toggler">
+        <i data-feather="menu"></i>
+    </a>
+    <div class="navbar-content">
+
+        <ul class="navbar-nav">
+            <li class="nav-item mx-3">
+                <a class="nav-link" href="/">
+                    {{-- <i data-feather="home"></i> --}}
+                    <span>HOME</span>
+                </a>
+            </li>
+            <li class="nav-item mx-3">
+                <a class="nav-link active" href="/peta">
+                    {{-- <i data-feather="map"></i> --}}
+                    <span>PETA</span>
+                </a>
+            </li>
+            <li class="nav-item mx-3">
+                <a class="nav-link" href="/grafik">
+                    {{-- <i data-feather="bar-chart-2"></i> --}}
+                    <span>GRAFIK</span>
+                </a>
+            </li>
+            <li class="nav-item mx-3">
+                <a class="nav-link" href="/edukasi">
+                    {{-- <i data-feather="book"></i> --}}
+                    <span>EDUKASI</span>
+                </a>
+            </li>
+            <li class="nav-item mx-3">
+                <a class="nav-link" href="/login">
+                    {{-- <i data-feather="book"></i> --}}
+                    <span>LOGIN</span>
+                </a>
+            </li>
+        </ul>
+
+    </div>
+</nav>
